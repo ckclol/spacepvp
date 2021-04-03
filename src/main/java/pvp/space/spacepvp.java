2f506ba078e5cb5aaded0f17206e30f6c3009162
tcfplayz <chau123ka89chun10p@gmail.com>
2021-04-03 19:32:25 +0800

    config

--- src/main/java/pvp/space/spacepvp.java
+++ src/main/java/pvp/space/spacepvp.java
@@ -1,14 +1,24 @@
 package pvp.space;
 
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class spacepvp extends JavaPlugin implements Listener, CommandExecutor {
 
+    FileConfiguration config = this.getConfig();
     @Override
     public void onEnable() {
-
+        config.addDefault("sumo", false);
+        config.addDefault("uhc", false);
+        config.addDefault("barrier", false);
+        config.addDefault("fly", true);
+        config.addDefault("(barrier) jump boost", 1);
+        config.addDefault("spd", false);
+        config.addDefault("spdvalue", 1);
+        config.options().copyDefaults(true);
+        this.saveConfig();
     }
 
     @Override
